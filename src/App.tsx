6b00eb473dbe306c4f408a2df381a0067fd1ff08
Ben Zhang <ben@ddns.office>
2024-02-06 16:57:31 +1100

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import YoutubeForm from "./components/YoutubeFor";
+
+function App() {
+  return (
+    <div>
+      <YoutubeForm />
+    </div>
+  );
+}
+
+export default App;
